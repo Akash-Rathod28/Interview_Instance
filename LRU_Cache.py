9d5c10acf3d8262c8a2c6b2511adb2435accfbaa
Akash Rathod <akash.rathod162844@gmail.com>
2026-03-03 07:03:31 +0530

    Create LRU_Cache.py

---
+++ LRU_Cache.py
@@ -0,0 +1,63 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # key -> node
+        
+        # Create dummy head and tail
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # Remove node from list
+    def remove(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    # Insert node right after head (MRU position)
+    def insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            
+            # Move to front (MRU)
+            self.remove(node)
+            self.insert(node)
+            
+            return node.value
+        
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Remove old node
+            self.remove(self.cache[key])
+        
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
+        
+        if len(self.cache) > self.capacity:
+            # Remove LRU node (node before tail)
+            lru = self.tail.prev
+            self.remove(lru)
+            del self.cache[lru.key]
